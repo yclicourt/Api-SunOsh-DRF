6b2dc8546e64625520aeb45e9edef5b050fb4da1
Yoan <yclicourt@gmail.com>
2023-10-08 04:21:50 -0400

    add middleware 2

--- sunosh/settings.py
+++ sunosh/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from datetime import timedelta
-from pathlib import Path
 import os
 import dj_database_url
+from datetime import timedelta
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'sunosh.urls'
